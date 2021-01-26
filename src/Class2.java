dd674bfcac8a01a12d1af47b8be575b3e8594ace
Veronika.Timkina <Veronika.Timkina@jetbrains.com>
2021-01-26 13:27:23 +0300

    changes in Class2

--- src/Class2.java
+++ src/Class2.java
@@ -1,2 +1,11 @@
 public class Class2 {
+
+    public static int a;
+
+    public static void main(String[] args) {
+        for (int i = 0; i < a; i++) {
+            System.out.println(i);
+        }
+    }
+
 }
